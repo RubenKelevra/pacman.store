b0cd46cbbfd2fc890ec05fa8c67f86e0ba6c9522
@RubenKelevra <cyrond@gmail.com>
2020-07-07 20:22:28 +0200

    loop_syncher: update to new script

--- toolset/loop_syncher.sh
+++ toolset/loop_syncher.sh
@@ -13,10 +13,10 @@ set -e
 # Usage: ./loop_syncher.sh --store-log
 
 
-[ ! -f './repo2cluster.sh' ] && exit 1
+[ ! -f './rsync2cluster.sh' ] && exit 1
 [ ! -f './status_mail.sh' ] && exit 1
 
-perm_logfile="./loop_syncher.log"
+perm_logfile="${HOME}/.rsync2cluster/loop_syncher.log"
 ERR_ON_LAST_RUN=0
 
 while true; do
@@ -25,7 +25,7 @@ while true; do
 	# remove temp file after this script ends
 	trap "rm -f '$tmp_file'" 0 2 3 15
 
-	if ! bash ./repo2cluster.sh > "$tmp_file" 2>&1; then
+	if ! bash ./rsync2cluster.sh > "$tmp_file" 2>&1; then
 		echo ""
 		bash ./status_mail.sh "error" "$tmp_file"
 		ERR_ON_LAST_RUN=1
